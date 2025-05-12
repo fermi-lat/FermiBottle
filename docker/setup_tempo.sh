ca71efaf0f0be003cb47d3a73422b22f619e69e2
James Amrhein <james.s.amrhein@nasa.gov>
2025-05-12 13:34:59 -0400

    Updated script to use a new tempo git repo that will compile on a aarch/arm64 platform.

--- docker/setup_tempo.sh
+++ docker/setup_tempo.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 TEMPO=$ASTROPFX/tempo
-git clone https://github.com/nanograv/tempo.git
+#git clone https://github.com/nanograv/tempo.git
+# git clone https://git.code.sf.net/p/tempo/tempo tempo-tempo
+git clone https://git.code.sf.net/p/tempo/tempo tempo
 mkdir -p $TEMPO
 cd tempo
 bash prepare
