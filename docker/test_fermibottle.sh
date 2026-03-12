1d39b1b9de018c41b0479ac444767608ae48a30c
James Amrhein <james.s.amrhein@nasa.gov>
2026-03-12 13:45:01 -0400

    Added more testing for FB

--- docker/test_fermibottle.sh
+++ docker/test_fermibottle.sh
@@ -10,24 +10,39 @@ CONDA_PATH="/opt/anaconda/lib/python3.12/site-packages/conda/shell/"    # Path t
 source "$CONDA_PATH/etc/profile.d/conda.sh" || echo "Conda path incorrect."
 # /opt/anaconda/lib/python3.12/site-packages/conda/shell/etc/profile.d/conda.sh
 
-mkdir -p /home/fermi/
-cd /home/fermi/
+mkdir -p /home/fermi/FB_TESTING
+cd /home/fermi/FB_TESTING/
 
 # --- Git Clone ---
 git clone "$REPO_URL"
+git clone "https://github.com/USRA-STI/gdt-fermi.git"
+git clone "https://github.com/fermi-lat/AnalysisThreads.git"
+git clone "https://github.com/FermiSummerSchool/fermi-summer-school.git"
 
 cd ScienceTools/
 # --- Create Directory ---
-mkdir -p test_results
+mkdir -p /home/fermi/FB_TESTING/test_results
 
 # Activate the environment
 conda activate "$ENV_NAME"
 conda install pmw junit-xml pytest
 
-cd test_results/
-pytest --junitxml=test_results/fermipy_results.xml -vv --pyargs fermipy
-../recipe/tests/ST-unit-test -w -d -v
-../recipe/tests/ST-AGN-thread-test -w -d -v
-
-
+cd /home/fermi/FB_TESTING/test_results/
+../ScienceTools/recipe/tests/ST-unit-test -w -d -v
+../ScienceTools/recipe/tests/ST-AGN-thread-test -w -d -v
+pytest --junitxml=fermipy_results.xml -vv --pyargs fermipy
+# pytest --junitxml=threeml_results.xml -vv --pyargs threeML
+cd ../fermi-summer-school/
+# cd ../fermi-summer-school/Likelihood_Advanced/
+# pytest --nbmake --nbmake-timeout=1000000 --junitxml=/home/fermi/FB_TESTING/test_results/fss_Likelihood_adv_result.xml -vv *.ipynb
+# cd ../Data_Exploration/
+# pytest --nbmake --nbmake-timeout=1000000 --junitxml=/home/fermi/FB_TESTING/test_results/data_quicklook_result.xml -vv pytest --nbmake --nbmake-timeout=10000 --junitxml=de_result2.xml -vv Data_Exploration/Data_Exploration.ipynb Data_QuickLook.ipynb 
+pytest --nbmake --nbmake-timeout=10000 --junitxml=/home/fermi/FB_TESTING/test_results/de_data_expl_result.xml  -vv Data_Exploration/Data_Exploration.ipynb 
 echo "Setup complete: Env $ENV_NAME activated, repo cloned, and test_results created."
+
+conda deactivate
+conda activate fermigdm
+cd /shared/TEST_BOTTLE/gdt-fermi/docs/notebooks
+for f in *.tar; do tar -xf "$f"; done
+pytest --nbmake --nbmake-timeout=10000 --junitxml=gdt_fermi_results.xml -vv *.ipynb
+echo "Setup complete: Env fermigbm"
