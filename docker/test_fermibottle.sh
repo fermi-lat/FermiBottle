038f122a0f3f38caa593ae4eb4024ec7f08f011d
James Amrhein <james.s.amrhein@nasa.gov>
2026-05-21 15:48:39 -0400

    FB tagged before Summer School

--- docker/test_fermibottle.sh
+++ docker/test_fermibottle.sh
@@ -15,16 +15,16 @@ mkdir -p /home/fermi/FB_TESTING/test_results
 
 cd /home/fermi/FB_TESTING/
 
-wget https://fermi.gsfc.nasa.gov/ssc/data/analysis/scitools/data/SummerSchool/Likelihood_rcw103.tgz
-wget https://fermi.gsfc.nasa.gov/ssc/data/analysis/scitools/data/SummerSchool/Likelihood_rxj1713.tgz
-tar -xvf Likelihood_rxj1713.tgz
-rm -f Likelihood_rxj1713.tgz
-cd Likelihood_rxj1713/
-pytest --nbmake -n auto --nbmake-timeout=6000 --junitxml=../FB_TESTING/test_results/ss_real_exe_rxj1713_result.xml -vv Realtime_execution_RXJ1713.ipynb
-cd ../
-tar -xvf Likelihood_rcw103.tgz
-cd Likelihood_rcw103/
-pytest --nbmake -n auto --nbmake-timeout=6000 --junitxml=../FB_TESTING/test_results/ss_real_exe_rcw103_result.xml -vv Realtime_execution_RCW103.ipynb
+# wget https://fermi.gsfc.nasa.gov/ssc/data/analysis/scitools/data/SummerSchool/Likelihood_rcw103.tgz
+# wget https://fermi.gsfc.nasa.gov/ssc/data/analysis/scitools/data/SummerSchool/Likelihood_rxj1713.tgz
+# tar -xvf Likelihood_rxj1713.tgz
+# rm -f Likelihood_rxj1713.tgz
+# cd Likelihood_rxj1713/
+# pytest --nbmake -n auto --nbmake-timeout=6000 --junitxml=../FB_TESTING/test_results/ss_real_exe_rxj1713_result.xml -vv Realtime_execution_RXJ1713.ipynb
+# cd ../
+# tar -xvf Likelihood_rcw103.tgz
+# cd Likelihood_rcw103/
+# pytest --nbmake -n auto --nbmake-timeout=6000 --junitxml=../FB_TESTING/test_results/ss_real_exe_rcw103_result.xml -vv Realtime_execution_RCW103.ipynb
 
 # --- Git Clone ---
 git clone "$REPO_URL"
@@ -44,8 +44,9 @@ conda install ipykernel nb_conda_kernels  astropy junit-xml pytest -y
 cd /home/fermi/FB_TESTING/test_results/
 ../ScienceTools/recipe/tests/ST-unit-test -w -d -v
 ../ScienceTools/recipe/tests/ST-AGN-thread-test -w -d -v
-pytest -n auto --junitxml=fermipy_results.xml -vv --pyargs fermipy
-pytest -n auto --junitxml=threeml_results.xml -vv --pyargs threeML
+pytest --junitxml=fermipy_results.xml -vv --pyargs fermipy
+pytest --junitxml=threeml_results.xml -vv --pyargs threeML
+# -n auto
 # pytest --junitxml=threeml_results.xml -vv --pyargs astromodels
 
 cd /home/fermi/FB_TESTING/AnalysisThreads/SourceAnalysis
@@ -76,6 +77,8 @@ pytest -n auto --nbmake --nbmake-timeout=5000 --junitxml=/home/fermi/FB_TESTING/
 
 ### cd ../fermi-summer-school/
 ### pytest --nbmake --nbmake-timeout=1000000 --junitxml=/home/fermi/FB_TESTING/test_results/fss_Likelihood_adv_result.xml -vv Likelihood_Advanced/*.ipynb
+
+
 cd /home/fermi/FB_TESTING/fermi-summer-school/
 pytest -n auto --nbmake --nbmake-timeout=3000 --junitxml=/home/fermi/FB_TESTING/test_results/data_quicklook_result.xml -vv Data_Exploration/*.ipynb
 pytest -n auto --nbmake --nbmake-timeout=3000 --junitxml=/home/fermi/FB_TESTING/test_results/find_source_results.xml -vv Advanced_Topics/FindSource/*.ipynb
